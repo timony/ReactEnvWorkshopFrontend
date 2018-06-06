d605ad9226dd1ca7c89ffc00a1f6629114150bdc
Mika Tomas <tomas.mika@tieto.com>
2018-06-06 10:04:41 +0200

    Injecting JS into html

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,8 +6,13 @@ import browserify from 'browserify';
 import del from 'del';
 import source from 'vinyl-source-stream';
 import gulpsync from 'gulp-sync';
+import inject from 'gulp-inject';
+import order from 'gulp-order';
+import path from 'path';
+import config from './build.config';
 
 const sync = gulpsync(gulp);
+
 
 gulp.task('clean', () => {
   return del(['dist']);
@@ -15,25 +20,50 @@ gulp.task('clean', () => {
 
 gulp.task('js', () => {
   const options = {
-    entries: './src/App.jsx',   // Entry point
+    entries: 'src/App.jsx',   // Entry point
     extensions: ['.js', '.jsx'],            // consider files with these extensions as modules
     debug: true,  // add resource map at the end of the file or not
-    paths: ['./src/']           // This allows relative imports in require, with './scripts/' as root
+    paths: ['src/']           // This allows relative imports in require, with './scripts/' as root
   };
 
   return browserify(options)
     .transform(babelify)
     .bundle()
     .pipe(source('app.js'))
-    .pipe(gulp.dest('./dist/js'));
+    .pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('transpile', sync.sync(['clean', ['js', 'html']]));
+gulp.task('vendor', () => {
+  return gulp
+    .src('vendor/*.js')
+    .pipe(gulp.dest('dist/vendor'));
+});
 
 gulp.task('html', () => {
-  gulp.src('src/index.html')
+  const target = gulp.src('src/*.html');
+  const sources = gulp.src([
+      'dist/css' + '/*.css',
+      'dist/js' + '/*.js',
+      'dist/vendor/*.js'
+    ],
+    {read: false}
+  );
+
+  const ordering = config.vendor.js.map(filePattern => path.basename(filePattern));
+  ordering.unshift('*.css');
+
+  const orderedSource = sources.pipe(order(ordering));
+
+  return target
+    .pipe(inject(orderedSource, {
+      ignorePath: 'dist',
+      addRootSlash: false
+    }))
     .pipe(gulp.dest('dist'));
+
 });
+
+gulp.task('transpile', sync.sync(['clean', 'js', 'vendor', 'html']));
 
 gulp.task('start-server', ['transpile'], () => {
   const connectOptions = {
